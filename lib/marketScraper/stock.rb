705716942f86fbf5f0d3d6ca6390fc7e664c0b17
JoeDeGr <59621264+JoeDeGr@users.noreply.github.com>
2020-04-03 21:21:50 -0400

    Update stock.rb

--- lib/marketScraper/stock.rb
+++ lib/marketScraper/stock.rb
@@ -1,9 +1,8 @@
 require 'pry'
 
-
 class Stock
     attr_accessor 
-    attr_reader :symbol, :name, :shares, :open, :high, :low, :value, :date, :volume
+    attr_reader :symbol, :name, :shares, :start, :high, :low, :value, :date, :volume
 
     @@all = []
 
@@ -33,36 +32,47 @@ class Stock
     end
 
     def info
-        Puts "Here is the info for #{self.name}"
-        puts self.shares
-        puts self.value
+        puts "Here is the info for #{self.name}"
+        
+        puts "You have #{self.shares} in your portfolio."
+        
+        puts "Each share is valued at #{self.value} per share."
+        
+        puts "The total value is #{self.total_value}."
+        
+        puts "On #{self.date} there was the following data:"
+        
+        puts "  Open:   #{self.start}"
+        puts "  High:   #{self.high}"
+        puts "   Low:   #{self.low}"
+        puts " Close:   #{self.value}"
+        puts "volume:   #{self.volume}"
     end
 
     def total_value 
-        total = self.value * self.shares
-        total.to_i
+        total = self.value.to_f * self.shares.to_f
+        binding.pry
+        total
     end 
 
     def get_info
 
-        # symbol = self.symbol
+        url = "http://eoddata.com/stockquote/NYSE/.htm"
 
-        # url = "http://eoddata.com/stockquote/NYSE/.htm"
+        doc = Nokogiri::HTML(open("http://eoddata.com/stockquote/NYSE/#{self.symbol}.htm"))
 
-        # doc = Nokogiri::HTML(open("http://eoddata.com/stockquote/NYSE/#{self.symbol}.htm"))
+        @date = doc.css("table.quotes tr:nth-child(2) > td:nth-child(1)").first.text.strip
+        @start =doc.css("table.quotes tr:nth-child(2) > td:nth-child(2)").first.text.strip
+        @high =doc.css("table.quotes tr:nth-child(2) > td:nth-child(3)").first.text.strip
+        @low = doc.css("table.quotes tr:nth-child(2) > td:nth-child(4)").first.text.strip
+        @value = doc.css("table.quotes tr:nth-child(2) > td:nth-child(5)").first.text.strip
+        @volume = doc.css("table.quotes tr:nth-child(2) > td:nth-child(6)").first.text.strip
 
-        # @date = doc.css("table.quotes tr:nth-child(2) > td:nth-child(1)").first.text.strip
-        # @open =doc.css("table.quotes tr:nth-child(2) > td:nth-child(2)").first.text.strip
-        # @high =doc.css("table.quotes tr:nth-child(2) > td:nth-child(3)").first.text.strip
-        # @low = doc.css("table.quotes tr:nth-child(2) > td:nth-child(4)").first.text.strip
-        # @value = doc.css("table.quotes tr:nth-child(2) > td:nth-child(5)").first.text.strip
-        # @volume = doc.css("table.quotes tr:nth-child(2) > td:nth-child(6)").first.text.strip
-
-        @date = 1
-        @open = 2
-        @high = 3
-        @low = 4
-        @value = 5
-        @volume =6
+        # @date = 15
+        # @start = 2
+        # @high = 3
+        # @low = 4
+        # @value = 5
+        # @volume =6
     end    
 end
